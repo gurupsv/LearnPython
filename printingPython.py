71881021757b52ceb4f5a3164d5e6386e7aae4d9
Guruprasad SV <guruprasad.sv@gmail.com>
2018-10-02 12:30:07 +0530

    First Commit

---
+++ printingPython.py
@@ -0,0 +1,33 @@
+# print ("This is {0} first print statement".format("Guru's"))
+# mystring = "Guruprasad loves Suneetha and Vaivaswatha!"
+# print (mystring[::1])
+# print (mystring[1::1])
+# print (mystring[1:-1:1])
+# print (mystring[1:-1:2])
+#
+# mystr1 = "Hello I am here!"
+# mystr2 = "Hello I am Here!"
+#
+# if mystr1 == mystr2:
+#     print ("They are Equal!!")
+# else:
+#     print ("They are not equal!!")
+
+
+# print("Enter your age ->")
+# myage=int(input())
+# if (18 <= myage <= 30):
+#     print("Welcome to 18-30 Holidays")
+# else:
+#     print("I am afraid, we cant let you in now!")
+
+number="Blah blah blah 2,44,654,78,534,908,733,5,23,44"
+onlynumber=''
+for i in number:
+    if i in "0123456789":
+        onlynumber=onlynumber+i
+
+print(onlynumber)
+
+for i in range(0,10,2):
+    print(i)
