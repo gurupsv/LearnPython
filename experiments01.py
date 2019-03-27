6eb5b4c869c484e6e09b16b703df3a9fb3a41174
gurupsv <guruprasad.sv@gmail.com>
2019-03-27 14:06:44 +0530

    Day 2

---
+++ experiments01.py
@@ -0,0 +1,22 @@
+def myfunc(arg1):
+    print(arg1)
+
+def myfunc_ret(arg1):
+    print(arg1)
+    return "somthing extra!!"
+
+print(myfunc("Hello"))
+print ("=========================")
+print (myfunc_ret("Good Bye!"))
+print ("=========================")
+
+def power(num, x=1):
+    result=1
+    for i in range(x):
+        result=result*num
+    return result
+
+print(power(2))
+print(power(2,3))
+print(power(x=3,num=4))
+print(power(1,2))
