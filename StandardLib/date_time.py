a0eb3d2b4b99fc92cfe762aa70bc06c31ee337b0
gurupsv <guruprasad.sv@gmail.com>
2019-03-27 20:09:31 +0530

    fixed datetime import error (file name cant be datetime)

--- StandardLib/date_time.py
+++ StandardLib/date_time.py
@@ -2,8 +2,12 @@ from datetime import date
 from datetime import time
 from datetime import datetime
 
-def main ():
+
+def main():
     today = date.today()
     print("Today's date is ", today)
     print ("Details ", today.day, today.month, today.year)
 
+
+if __name__ == "__main__":
+    main()
