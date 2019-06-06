11337bcaebf57c508b5eb878dd215767b5d91ea2
Guruprasad Vidyaranyapura <guruprasad.sv@gmail.com>
2019-06-06 21:53:37 +0530

    Exceptions

--- Training/python_init_file_parsing.py
+++ Training/python_init_file_parsing.py
@@ -3,5 +3,5 @@ config = configparser.ConfigParser()
 config.read('examples.ini')
 print(config.sections())
 print(config["database"]["User"])
-print(config["database"]["Compression"])
+print(config["chat"]["Port"])
 #dict of dict.
