dc24eada72902e74d3ac4a4154e871541abb18b4
Guruprasad Shringeri Vidyaranyapura <guruprasad.vidyaranyapura@oracle.com>
2024-01-11 23:11:49 +0530

    write config parser for oci

--- OracleCloudInfra/ociconfig.py
+++ OracleCloudInfra/ociconfig.py
@@ -1,36 +1,56 @@
 import configparser
 import os
+import argparse
 
-
-def parse_config(file_path):
+def parse_config(file_path, selected_section=None, selected_key=None):
     config = configparser.ConfigParser()
     config.read(file_path)
 
-    print("Available Sections:")
-    for section in config.sections():
-        print(f"- {section}")
+    if selected_key:
+        print(f"All Sections and Key-Value Pairs for Key '{selected_key}':")
+        for section in config.sections():
+            if config.has_option(section, selected_key):
+                value = config.get(section, selected_key)
+                print(f"\n[{section}] {selected_key}={value}")
 
-    selected_section = input("Enter the section you want to print (or press Enter to print all): ")
+    elif selected_section is None:
+        print("\nAvailable Sections:")
+        for section in config.sections():
+            print(f"- {section}")
 
-    if selected_section:
-        if config.has_section(selected_section):
+    selected_section = input("\nEnter the section you want to print (or press Enter to exit): ")
+
+    if not selected_section:
+        print("Exiting.")
+        return
+
+    if config.has_section(selected_section):
+        #if not selected_key:
             print(f"\n[{selected_section}]")
             for key, value in config.items(selected_section):
                 print(f"{key}={value}")
-        else:
-            print(f"Section '{selected_section}' not found.")
+        #else:
+        #    if config.has_option(selected_section, selected_key):
+        #        value = config.get(selected_section, selected_key)
+         #       print(f"\n[{selected_section}] {selected_key}={value}")
+         #   else:
+          #      print(f"Key '{selected_key}' not found in section '{selected_section}'.")
     else:
-        for section in config.sections():
-            print(f"\n[{section}]")
-            for key, value in config.items(section):
-                print(f"{key}={value}")
-
+        print(f"Section '{selected_section}' not found.")
 
 if __name__ == "__main__":
     home_dir = os.environ.get("HOME")
     file_path = os.path.join(home_dir, ".oci/config")
 
     if os.path.isfile(file_path):
-        parse_config(file_path)
+        parser = argparse.ArgumentParser(description="Parse and print sections from config file")
+        parser.add_argument("--section", help="Specify the section to print")
+        parser.add_argument("--key", help="Specify the key to print within the section")
+
+        args = parser.parse_args()
+        selected_section = args.section
+        selected_key = args.key
+
+        parse_config(file_path, selected_section, selected_key)
     else:
         print(f"Config file not found: {file_path}")
